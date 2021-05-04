8603da7973e634213bfd2bfa1dea14648298bdd1
Isac Almeida <36626000+IAlmeidaCE-23@users.noreply.github.com>
2021-05-04 13:45:40 -0300

    Some new funcionalities
    Changes for optional directories and command args

--- Treenamer/Program.cs
+++ Treenamer/Program.cs
@@ -5,34 +5,76 @@ namespace Treenamer
 {
     class Program
     {
+        private static string directory = string.Empty, find = string.Empty, replace = string.Empty, content = string.Empty;
+
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            Header();
+
+            if (args.Length != 4)
             {
-                Console.WriteLine(
-@"
-Treenamer: 
-Recursively finds and replaces strings on file and directory names based on a root directory.
-Optionally finds and replaces in file contents.
-
-Usage: trename {directory} {string} {replacement} [-c]
-
-Parameters:
-    -directory: directory to run this tool
-    -string: string to be replaced
-    -replacement: replacement for the string
-    -c (optional): also finds and replaces file contents
-");
-
+                ManualRun(out directory, out find, out replace, out content);
             }
-            var directory = args[0];
-            var find = args[1];
-            var replace = args[2];
-            var replaceContent = args.Length >= 4 && args[3] == "-c";
-            new RecursiveReplacer(directory, find, replace, replaceContent).RecursiveReplace();
-
+            else
+            {
+                directory = args[0];
+                find = args[1];
+                replace = args[2];
+                content = args[3];
+                ConfirmAndRun();
+            }
         }
 
-        
+        private static void ConfirmAndRun()
+        {
+            Console.WriteLine("Confirm replace with parameters? " + directory + " " + find + " " + replace + " " + content);
+            Console.WriteLine("Press enter for start!!!");
+            Run(directory, find, replace, content);
+        }
+
+        private static void Run(string directory, string find, string replace, string content)
+        {
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                new RecursiveReplacer(directory, find, replace, (content == "a" || content == "c")).RecursiveReplace();
+                Header();
+                ManualRun(out directory, out find, out replace, out content);
+            }
+            else
+            {
+                Header();
+                ManualRun(out directory, out find, out replace, out content);
+            }
+        }
+
+        private static void ManualRun(out string directory, out string find, out string replace, out string content)
+        {
+            Console.Write(
+                                @"Usage: trename {directory} {string} {replacement} [f|c|a]
+                    
+                    Parameters:
+                        -directory: directory to run this tool
+                        -string to be replaced
+                        -replacement for the string
+                        -o: f - files, c - contents, a - all   - type of replace executions
+                    ");
+
+            Console.WriteLine("Directory:");
+            directory = Console.ReadLine();
+            Console.WriteLine("Term to find:");
+            find = Console.ReadLine();
+            Console.WriteLine("Term to replace:");
+            replace = Console.ReadLine();
+            Console.WriteLine("Level of replace (F - files, C - contents, a - All):");
+            content = Console.ReadLine();
+            ConfirmAndRun();
+        }
+
+        private static void Header()
+        {
+            Console.Write(@"------------------------------------------------------------------------------------------" + Environment.NewLine + 
+                              "Treenamer - Ease rename for solutions" + Environment.NewLine + 
+                              "Recursively finds and replaces strings on file and directory names based on a root directory." + Environment.NewLine + Environment.NewLine + Environment.NewLine);
+        }
     }
 }
